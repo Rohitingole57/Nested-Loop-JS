1f8cd1ec3463ee00e3af71c5406ff2c668e4e80e
Rohit Ingole <ingolerohit2017@gmail.com>
2023-04-03 13:24:44 +0530

    nested loop

---
+++ index.js
@@ -0,0 +1,9 @@
+// nested loop ====> Loop inside the loops is called as nested loop.
+
+for(var i=1; i<=5; i++){
+   for(var j=1; j<=5; j++)
+   {
+       console.log("kaise ho !!!!!!!")
+   }
+   console.log("----------------------------------------------------------")
+}
